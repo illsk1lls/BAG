76bc786be010a618143c6fd7fbf9aa085939432b
illsk1lls <gene.rovinsky@gmail.com>
2023-06-27 23:10:59 -0400

    Prioritized Multi-Line BAG over single line version

--- README.md
+++ README.md
@@ -1,19 +1,17 @@
 # BAG - A command that uses its own wrapper to store/extract files.
 
-BAG is a frivolous puzzle. A waste of time.. How much of the multi-line BAG can we get to run on a single line?? We may never know ;P
+
 
 Drag and drop an item onto 'emptyBAG.cmd' to place it inside the bag. Double click 'fullBAG.cmd' to empty the bag.
 
-IMPORTANT! ~70mb max file size allowed, for single line bag, at present NO SPACES OR SPECIAL CHARS ("()!@#$%^&*-+;:') in filenames/paths allowed.
-*Multi-Line BAG supports special chars and spaces. 
+~70mb max file size allowed. 
 
 Single file only (can be anything)
 
 CAUTION! 
 Make sure to only use test COPIES of files and not originals when testing BAG. 
-BAG deletes the original file, because thats the point. You are putting the file in the BAG.
+BAG deletes the original file, because thats the point. You are putting the file IN the BAG.
 It is possible to lose the file you drop if an error occurs, so DO NOT test with original data that is not backed up.
-
 
 BAG uses Powershell and CertUtil.exe to convert any file to BASE64 and add it to itself. It then deletes the original file and renames itself to 'fullBAG.cmd'
 
@@ -22,4 +20,5 @@ The process works in reverse for fullBAG.cmd
 When fullBAG is clicked, the file is converted from BASE64 back to its original state with its original name, and fullBAG
 exports itself back into emptyBAG.cmd and deletes itself.
 
-Extra care and unnecessary stress has been endured to ensure BAG is able to run as a single line of code ;)
+Single line BAG is just for fun and a frivolous puzzle. A waste of time.. How much of the multi-line BAG can we get to run on a single line?? We may never know ;P
+*For single line bag, at present NO SPACES OR SPECIAL CHARS ("()!@#$%^&*-+;:') in filenames/paths allowed.
--- emptyBAG.cmd
+++ emptyBAG.cmd
@@ -1,2 +1,37 @@
-@ECHO OFF & CD /D "%~dp0" && (IF EXIST "%temp%\getadmin.vbs" DEL "%temp%\getadmin.vbs") && fsutil dirty query %systemdrive% 1>NUL 2>NUL || (ECHO Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k CD ""%~sdp0"" && %~s0 %1", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && EXIT /B) & IF NOT [%1]==[] ((IF "%~n0"=="fullBAG" EXIT) & ECHO FILLING BAG... & CERTUTIL -ENCODE -F """%1""" """%TEMP%\%~n1.000""">nul & powershell -nop -c ^(Get-Content """%TEMP%\%~n1.000"""^) -replace '^^^','::' -replace '-----BEGIN CERTIFICATE-----','%~n1%~x1::' -replace '-----END CERTIFICATE-----','%~n1%~x1::' ^| Set-Content """%TEMP%\%~n1.001""" & DEL "%TEMP%\%~n1.000" /F /Q>nul & powershell -nop -c Add-Content -Path """%~f0""" -Value ^(Get-Content -Path """%TEMP%\%~n1.001"""^) & DEL "%TEMP%\%~n1.001" /F /Q>nul & DEL "%1" /F /Q>nul & COPY /Y "%~f0" "%~dp0fullBAG.cmd" & GOTO 2>nul & del "%~f0" & EXIT) ELSE ((IF "%~n0"=="emptyBAG" EXIT) & ECHO EMPTYING BAG... & powershell -nop -c $file=Get-Content '%~f0'; $r=[regex]::Match^($file,'\:\:^([^^^\:]+^)\:\:^(.+?^)\:\:\1\:\:'^).Groups[2].Value.Replace^(' ',"""`n"""^).Replace^('::',''^) ^| Set-Content '%TEMP%\%~n1.003' & powershell -nop -c ^(Get-Content """%~f0""" ^| Select-Object -Skip 1^) ^| Set-Content """%TEMP%\%~n1.002""" & set /p FNAME=<"%TEMP%\%~n1.002" & DEL "%TEMP%\%~n1.002" /F /Q>nul & SETLOCAL ENABLEDELAYEDEXPANSION & CERTUTIL -DECODE -F "%TEMP%\%~n1.003" "!FNAME::=!">nul & ENDLOCAL & DEL "%TEMP%\%~n1.003" /F /Q>nul & powershell -nop -c ^(get-content """%~f0""" -totalcount 1^) ^| set-content """%~dp0emptyBAG.cmd""" & GOTO 2>nul & del "%~f0" & EXIT)
+@ECHO OFF & SET "PARAM=%1" & SET "PARAMS=%*"
+SET "PARAM=%PARAM:"=%
+SET "PARAMS=%PARAMS:"=%
+IF NOT "%PARAM%" == "%PARAMS%" ECHO You can only put one thing at a time in the BAG! & PAUSE & EXIT /b
+>nul 2>&1 reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %*
+>nul 2>&1 fltmc|| if "%f0%" neq "%~f0" (cd.>"%temp%\runas.Admin" & start "%~n0" /high "%temp%\runas.Admin" "%~f0" "%_:"=""%" & exit /b)
+>nul 2>&1 reg delete hkcu\software\classes\.Admin\ /f
+>nul 2>&1 del %temp%\runas.Admin /f /q 
+IF NOT [%1]==[] (CALL :FILLBAG %1) ELSE (CALL :EMPTYBAG)
+EXIT
+:EMPTYBAG
+IF "%~n0"=="emptyBAG" ECHO The BAG is already empty, drag-and-drop something onto the BAG to put it inside. ;^) & PAUSE & EXIT /b
+ECHO EMPTYING BAG...
+powershell -nop -c $file=Get-Content '%~f0'; $r=[regex]::Match^($file,'\:\:^([^^^\:]+^)\:\:^(.+?^)\:\:\1\:\:'^).Groups[2].Value.Replace^(' ',"""`n"""^).Replace^('::',''^) ^| Set-Content '%TEMP%\%~n0.000' >nul
+powershell -nop -c ^(Get-Content '%~f0' ^| Select-Object -Skip 36^) ^| Set-Content '%TEMP%\%~n0.001' >nul
+SET /p FNAME=<"%TEMP%\%~n0.001" 
+DEL "%TEMP%\%~n0.001" /F /Q>nul
+SETLOCAL ENABLEDELAYEDEXPANSION
+CERTUTIL -DECODE -F "%TEMP%\%~n0.000" "%~dp0!FNAME::=!" >nul
+ENDLOCAL
+DEL "%TEMP%\%~n0.000" /F /Q>nul
+powershell -nop -c ^(get-content '%~f0' -totalcount 36^) ^| set-content '%~dp0emptyBAG.cmd' >nul
+GOTO 2>nul & del "%~f0" /F /Q>nul & EXIT /b
+:FILLBAG
+IF NOT %~z1 GEQ 1 (ECHO Empty files not allowed! & PAUSE & EXIT /b) ELSE (IF %~z1 GEQ 74400000 ECHO File Exceeds Safe Size Limit! ^(~70mb^) & PAUSE & EXIT /b)
+IF EXIST %1\* ECHO Folders are not supported! Single file only.. & PAUSE & EXIT /b
+IF "%~n0"=="fullBAG" ECHO The BAG is already full! Open the BAG to empty it. & PAUSE & EXIT /b
+ECHO FILLING BAG...
+CERTUTIL -ENCODE -F %1 "%TEMP%\%~n1.000">nul
+powershell -nop -c ^(Get-Content '%TEMP%\%~n1.000'^) -replace '^^^','::' -replace '-----BEGIN CERTIFICATE-----','%~nx1::' -replace '-----END CERTIFICATE-----','%~nx1::' ^| Set-Content '%TEMP%\%~n1.001' >nul
+DEL "%TEMP%\%~n1.000" /F /Q>nul
+powershell -nop -c Add-Content -Path '%~f0' -Value ^(Get-Content -Path '"%TEMP%\%~n1.001"'^) >nul
+DEL "%TEMP%\%~n1.001" /F /Q>nul
+DEL %1 /F /Q>nul
+COPY /Y "%~f0" "%~dp0fullBAG.cmd" >nul
+GOTO 2>nul & del "%~f0" /F /Q>nul & EXIT /b
 ::
