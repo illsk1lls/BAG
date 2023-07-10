f2b720796d97ac9d01d2f46cf41433006e77849c
illsk1lls <gene.rovinsky@gmail.com>
2023-07-10 01:15:49 -0400

    Update emptyBAG.cmd
    Removed the need for temp files.
    Removed the need for CertUtil.exe
    Removed file size limitations.
    *Large files take a while to extract, be patient.

--- emptyBAG.cmd
+++ emptyBAG.cmd
@@ -5,33 +5,22 @@ IF NOT "%PARAM%" == "%PARAMS%" ECHO You can only put one thing at a time in the 
 >nul 2>&1 reg add hkcu\software\classes\.Admin\shell\runas\command /f /ve /d "cmd /x /d /r set \"f0=%%2\"& call \"%%2\" %%3"& set _= %*
 >nul 2>&1 fltmc|| if "%f0%" neq "%~f0" (cd.>"%temp%\runas.Admin" & start "%~n0" /high "%temp%\runas.Admin" "%~f0" "%_:"=""%" & exit /b)
 >nul 2>&1 reg delete hkcu\software\classes\.Admin\ /f
->nul 2>&1 del %temp%\runas.Admin /f /q 
+>nul 2>&1 del %temp%\runas.Admin /f /q
 IF NOT [%1]==[] (CALL :FILLBAG %1) ELSE (CALL :EMPTYBAG)
 EXIT
-:EMPTYBAG
-IF "%~n0"=="emptyBAG" ECHO The BAG is already empty, drag-and-drop something onto the BAG to put it inside. ;^) & PAUSE & EXIT /b
-ECHO EMPTYING BAG...
-powershell -nop -c $file=Get-Content '%~f0'; $r=[regex]::Match^($file,'\:\:^([^^^\:]+^)\:\:^(.+?^)\:\:\1\:\:'^).Groups[2].Value.Replace^(' ',"""`n"""^).Replace^('::',''^) ^| Set-Content '%TEMP%\%~n0.000' >nul
-powershell -nop -c ^(Get-Content '%~f0' ^| Select-Object -Skip 36^) ^| Set-Content '%TEMP%\%~n0.001' >nul
-SET /p FNAME=<"%TEMP%\%~n0.001" 
-DEL "%TEMP%\%~n0.001" /F /Q>nul
-SETLOCAL ENABLEDELAYEDEXPANSION
-CERTUTIL -DECODE -F "%TEMP%\%~n0.000" "%~dp0!FNAME::=!" >nul
-ENDLOCAL
-DEL "%TEMP%\%~n0.000" /F /Q>nul
-powershell -nop -c ^(get-content '%~f0' -totalcount 36^) ^| set-content '%~dp0emptyBAG.cmd' >nul
-GOTO 2>nul & del "%~f0" /F /Q>nul & EXIT /b
 :FILLBAG
-IF NOT %~z1 GEQ 1 (ECHO Empty files not allowed! & PAUSE & EXIT /b) ELSE (IF %~z1 GEQ 74400000 ECHO File Exceeds Safe Size Limit! ^(~70mb^) & PAUSE & EXIT /b)
+IF NOT %~z1 GEQ 1 (ECHO Empty files not allowed! & PAUSE & EXIT /b)
 IF EXIST %1\* ECHO Folders are not supported! Single file only.. & PAUSE & EXIT /b
 IF "%~n0"=="fullBAG" ECHO The BAG is already full! Open the BAG to empty it. & PAUSE & EXIT /b
 ECHO FILLING BAG...
-CERTUTIL -ENCODE -F %1 "%TEMP%\%~n1.000">nul
-powershell -nop -c ^(Get-Content '%TEMP%\%~n1.000'^) -replace '^^^','::' -replace '-----BEGIN CERTIFICATE-----','%~nx1::' -replace '-----END CERTIFICATE-----','%~nx1::' ^| Set-Content '%TEMP%\%~n1.001' >nul
-DEL "%TEMP%\%~n1.000" /F /Q>nul
-powershell -nop -c Add-Content -Path '%~f0' -Value ^(Get-Content -Path '"%TEMP%\%~n1.001"'^) >nul
-DEL "%TEMP%\%~n1.001" /F /Q>nul
+ECHO.>>"%~f0" & ECHO :^:%~nx1:^:>>"%~f0" & ECHO|SET /p="::">>"%~f0" & POWERSHELL -nop -c "[Convert]::ToBase64String([IO.File]::ReadAllBytes(\"%1\"))">>"%~f0" & ECHO :^:%~nx1:^:>>"%~f0"
 DEL %1 /F /Q>nul
 COPY /Y "%~f0" "%~dp0fullBAG.cmd" >nul
 GOTO 2>nul & del "%~f0" /F /Q>nul & EXIT /b
-::
+EXIT /b 
+:EMPTYBAG
+IF "%~n0"=="emptyBAG" ECHO The BAG is already empty, drag-and-drop something onto the BAG to put it inside. ;^) & ECHO. & PAUSE & EXIT /b
+IF %~z0 GEQ 80000000 (ECHO EMPTYING BAG... ^(This may take a while^)) ELSE (ECHO EMPTYING BAG...)
+POWERSHELL -nop -c $file=Get-Content '%~f0'; $name=[regex]::Match^($file,'\:\:^([^^^\:]+^)\:\:^(.+?^)\:\:\1\:\:'^).Groups[1].Value.Replace^('::',''^).Trim^(^); $data=[regex]::Match^($file,'\:\:^([^^^\:]+^)\:\:^(.+?^)\:\:\1\:\:'^).Groups[2].Value.Replace^('::',''^).Trim^(^); [IO.File]::WriteAllBytes(\"$name\", [Convert]::FromBase64String($data)); ^(get-content '%~f0' -totalcount 26^) ^| set-content '%~dp0emptyBAG.cmd' >nul
+GOTO 2>nul & del "%~f0" /F /Q>nul & EXIT /b
+EXIT /b
