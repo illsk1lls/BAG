ac7d1893bd51e44496d9d225827f4486b66ef0af
illsk1lls <gene.rovinsky@gmail.com>
2023-07-13 18:44:26 -0400

    Update BAG.cmd
    BAG will only run on supported systems. Win 10+

--- BAG.cmd
+++ BAG.cmd
@@ -1,8 +1,10 @@
-@ECHO OFF & SET N=0 
+@ECHO OFF & SET N=0
 >nul 2>&1 REG ADD HKCU\Software\Classes\.Admin\shell\runas\command /f /ve /d "CMD /x /d /r SET \"f0=%%2\"& call \"%%2\" %%3"& SET _= %*
 >nul 2>&1 FLTMC|| IF "%f0%" NEQ "%~f0" (CD.>"%temp%\runas.Admin" & START "%~n0" /high "%temp%\runas.Admin" "%~f0" "%_:"=""%" & EXIT /b)
 >nul 2>&1 REG DELETE HKCU\Software\Classes\.Admin\ /f
 >nul 2>&1 DEL %temp%\runas.Admin /f
+FOR /F "usebackq skip=2 tokens=3-4" %%i IN (`REG QUERY "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName 2^>nul`) DO set "ProductName=%%i %%j"
+IF NOT "%ProductName%"=="Windows 10" ECHO. & ECHO UNSUPPORTED SYSTEM DETECTED! & ECHO. & PAUSE & EXIT
 IF [%1]==[] (CALL :EMPTYBAG) ELSE (ECHO FILLING BAG... & ECHO. & FOR %%i IN (%*) DO (CALL :FILLBAG %%i))
 IF %N%==1 (ECHO. & PAUSE & EXIT) ELSE (EXIT)
 :FILLBAG
@@ -14,7 +16,7 @@ IF %Size% GEQ 1000000000 ECHO [    WARNING!    ] - %~nx1 - Not Added! There is n
 ECHO.>>"%~f0" & POWERSHELL -nop -c "Add-Content '%~f0' """:^:%~nx1:^:""" -NoNewline; [Convert]::ToBase64String([IO.File]::ReadAllBytes("""%~1""")) | Add-Content """%~f0""" -NoNewline; Add-Content '%~f0' """:^:%~nx1:^:""" -NoNewline" & DEL /F "%~1">nul
 EXIT /b
 :EMPTYBAG
-IF %~z0 LSS 1965 ECHO The BAG is already empty, drag-and-drop something onto the BAG to put it inside. ;^) & ECHO. & PAUSE & EXIT /b
+IF %~z0 LSS 2220 ECHO The BAG is already empty, drag-and-drop something onto the BAG to put it inside. ;^) & ECHO. & PAUSE & EXIT /b
 IF %~z0 GEQ 80000000 (ECHO EMPTYING BAG... ^(This may take a while^)) ELSE (ECHO EMPTYING BAG...)
-POWERSHELL -nop -c "$file=Get-Content '%~f0'; $match=[regex]::Matches($file,'\:\:([^^\:]+)\:\:(.+?)\:\:\1\:\:') | Foreach-Object {$name=$_.Groups[1].Value; $fname=$name; while(Test-Path -Path """%~dp0$fname""") { $n++; $fname="""($n)$name""" }; $data=$_.Groups[2].Value; [IO.File]::WriteAllBytes("""$fname""", [Convert]::FromBase64String($data))}; (Get-Content '%~f0' -TotalCount 20) | Set-Content '%~f0'">nul
+POWERSHELL -nop -c "$file=Get-Content '%~f0'; $match=[regex]::Matches($file,'\:\:([^^\:]+)\:\:(.+?)\:\:\1\:\:') | Foreach-Object {$name=$_.Groups[1].Value; $fname=$name; while(Test-Path -Path """%~dp0$fname""") { $n++; $fname="""($n)$name""" }; $data=$_.Groups[2].Value; [IO.File]::WriteAllBytes("""$fname""", [Convert]::FromBase64String($data))}; (Get-Content '%~f0' -TotalCount 22) | Set-Content '%~f0'">nul
 EXIT /b
